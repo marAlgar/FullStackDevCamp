2b5c266436b58c0719602bb4ea65e432bea9606e
Marcela Alvarez <124836119+marAlgar@users.noreply.github.com>
2024-02-13 12:07:03 +0000

    module-2

---
+++ module-2/dictionary.py
@@ -0,0 +1,13 @@
+players = {
+	"ss": "Correa",
+	"2b": "Altuve",
+	"3b": "Bregman",
+	"DH": "Gattis",
+	"OF": "Springer"
+}
+
+second_base = players['2b']
+designated_hitter = players['DH']
+
+print(designated_hitter)
+print(second_base)
---
+++ module-2/histogram.py
@@ -0,0 +1,14 @@
+# Write a program that prints a histogram of a
+# dictionary of sales data.
+
+sales = {
+	'google': 20,
+	'facebook': 42,
+	'twitter': 10,
+	'offline': 12,
+}
+
+print('google ' + sales['google'] * '$')
+print('facebook ' + sales['facebook'] * '$')
+print('twitter ' + sales['twitter'] * '$')
+print('offline ' + sales['offline'] * '$')
