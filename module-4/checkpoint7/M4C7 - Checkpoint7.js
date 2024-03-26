6943910d8a6395179531abeefcfebb370d3da691
Marcela Alvarez <124836119+marAlgar@users.noreply.github.com>
2024-03-26 11:27:58 +0000

    checkpoint7

--- module-4/checkpoint7/M4C7 - Checkpoint7.js
+++ module-4/checkpoint7/M4C7 - Checkpoint7.js
@@ -1,13 +1,10 @@
 function operation(num1, num2, num3, num4) {
 	// Suma los dos primeros argumentos
-	var first_sum = num1 + num2;
+	var sum1 = num1 + num2;
 	// Suma los dos segundos argumentos
-	var second_sum = num3 + num4;
-	// Multiplica las dos sumas
-	var result = first_sum * second_sum;
-  
-	// Compara el resultado con 50
-	if (result > 50) {
+	var sum2 = num3 + num4;
+	// Compara el resultado de la multiplicación con 50
+	if ((sum1 * sum2) >= 50) {
 	  console.log("¡El número es mayor que 50!");
 	} else {
 	  console.log("¡El número es menor que 50!");
